d76a6e01ab92c5fb3874e57bdd8273d8f7b2be17
Javier Reyes <javier.reys@gmail.com>
2021-02-08 18:09:15 -0300

    feat: deploy in universal mode.

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ApiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `https://my-json-server.typicode.com/JaavierR/real-world-nuxt`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
